68f8308057cde17f7e5da733bc023826d03a4dc8
arturfilipd <arturfilipd@gmail.com>
2020-06-21 13:25:57 +0200

    authorization system implemented

--- src/main/java/com/AirlinesApp/Model/Role.java
+++ src/main/java/com/AirlinesApp/Model/Role.java
@@ -1,22 +1,39 @@
 package com.AirlinesApp.Model;
 
 import javax.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
 @Table(name = "roles")
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "role_id")
-    private int id;
-    @Column(name = "role")
-    private String role;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/AirlinesApp/Repository/UserRepository.java
+++ src/main/java/com/AirlinesApp/Repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.AirlinesApp.Repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.AirlinesApp.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
-
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
