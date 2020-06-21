c651cbbddcdab2c7c3022474ce9f30853642574b
arturfilipd <arturfilipd@gmail.com>
2020-06-21 10:18:49 +0200

    init

---
+++ src/test/java/com/AirlinesApp/AirlinesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AirlinesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
